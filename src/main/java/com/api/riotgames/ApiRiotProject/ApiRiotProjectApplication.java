224fdd3e6ee26b7c64f39175957aa5580b275cc9
LucasQ <mallevo13@gmail.com>
2019-01-29 22:51:56 -0200

    project config

---
+++ src/main/java/com/api/riotgames/ApiRiotProject/ApiRiotProjectApplication.java
@@ -0,0 +1,14 @@
+package com.api.riotgames.ApiRiotProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRiotProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRiotProjectApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/api/riotgames/ApiRiotProject/ApiRiotProjectApplicationTests.java
@@ -0,0 +1,17 @@
+package com.api.riotgames.ApiRiotProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiRiotProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
